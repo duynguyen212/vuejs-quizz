c7b4401e73895879395c238c46be515d475c437d
Duy NGUYEN <duynguyen212@gmail.com>
2021-02-03 15:43:04 +0100

    Quizz App

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-nav class="mynav">
+      <b-nav-item disabled><strong>Fancy Quiz App</strong></b-nav-item>
+      <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>      
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: [
+    'numCorrect',
+    'numTotal'
+  ]
+  
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .mynav {
+    height: 50px;
+    align-items: center;
+  }
+</style>
